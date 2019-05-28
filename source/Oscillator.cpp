4a9b5cac56a7d41e7148b2d1d32c3a02b46d8796
yannick-1 <50972269+yannick-1@users.noreply.github.com>
2019-05-29 09:20:07 +1200

    Add files via upload

---
+++ source/Oscillator.cpp
@@ -0,0 +1,43 @@
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "Oscillator.h"
+
+Oscillator::Oscillator(JuceSynthFrameworkAudioProcessor& p):
+    processor(p)
+{
+    setSize (200, 200);
+    
+    oscMenu.addItem ("Sine", 1);
+    oscMenu.addItem ("Saw", 2);
+    oscMenu.addItem ("Square", 3);
+    oscMenu.setJustificationType (Justification::centred);
+    addAndMakeVisible (&oscMenu);
+    oscMenu.addListener (this);
+    
+    waveSelection = new AudioProcessorValueTreeState::ComboBoxAttachment (processor.tree, "wavetype", oscMenu);
+}
+
+Oscillator::~Oscillator() {}
+
+void Oscillator::paint (Graphics& g)
+{
+    Rectangle<int> titleArea (0, 10, getWidth(), 20);
+    
+    g.fillAll (Colours::black);
+    g.setColour (Colours::white);
+    g.drawText ("Oscillator 1", titleArea, Justification::centredTop);
+    
+    Rectangle<float> area (25, 25, 150, 150);
+    
+    g.setColour (Colours::yellow);
+    g.drawRoundedRectangle (area, 20.0f, 2.0f);
+}
+
+void Oscillator::resized()
+{
+    Rectangle<int> area = getLocalBounds().reduced(40);
+    
+    oscMenu.setBounds (area.removeFromTop (20));
+}
+
+void Oscillator::comboBoxChanged (ComboBox* box) {}
---
+++ source/PluginEditor.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+#include "Oscillator.h"
+#include "Envelope.h"
+#include "Filter.h"
+
+
+class JuceSynthFrameworkAudioProcessorEditor:
+    public AudioProcessorEditor
+
+{
+    public:
+        JuceSynthFrameworkAudioProcessorEditor (JuceSynthFrameworkAudioProcessor&);
+        ~JuceSynthFrameworkAudioProcessorEditor();
+
+        void paint (Graphics&) override;
+        void resized() override;
+    
+
+    private:
+        // This reference is provided as a quick way for your editor to
+        // access the processor object that created it.
+        JuceSynthFrameworkAudioProcessor& processor;
+    
+        Oscillator oscGui;
+        Envelope   envGui;
+        Filter     filterGui;
+
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (JuceSynthFrameworkAudioProcessorEditor)
+};
